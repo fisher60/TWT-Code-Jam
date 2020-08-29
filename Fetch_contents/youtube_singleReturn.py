58085e4dacc04966b3f27798c942d8783dc71aab
NMarco <53913990+marco-create@users.noreply.github.com>
2020-08-29 20:57:57 +0200

    Add files via upload

---
+++ Fetch_contents/youtube_singleReturn.py
@@ -0,0 +1,29 @@
+from googleapiclient.discovery import build
+
+api_key = 'AIzaSyAhgEI6eSn4hly9lp3wh1vvRjt3fDtV9Tk'
+youtube = build('youtube', 'v3', developerKey= api_key)
+
+def yt_searchtopic(topic, key=api_key, build_yt=youtube):
+
+	request = youtube.search().list(
+    part='snippet',
+    maxResults=10,
+    order='date',
+    q=topic,
+    relevanceLanguage='en')
+
+	response = request.execute()	## stored as dictionary
+	response_extracted = response['items']	## extract this key
+
+	vids_urls = []
+	for r in response_extracted[:]:
+		vid_url = r['id']['videoId']	## 'videoId' location in each response dictionary'\
+		vid_title = r['snippet']['title']
+
+		vids_urls.append(['https://www.youtube.com/watch?v=' + vid_url,vid_title])
+	return vids_urls
+
+if __name__ == '__main__':
+	#if running this file separately, ask the user for the topic
+	topic = input('>> Insert topic: ')
+	print(yt_searchtopic(topic))
